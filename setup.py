2c4977e47a58ffa1821ee9b0cd6c8e7f1ae3c74a
Martin Peeters <martin.peeters@affinitic.be>
2019-06-25 11:59:21 +0200

    Back to development: 1.0a3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.ws.register",
-    version="1.0a2",
+    version="1.0a3.dev0",
     description="Route subscription for webservice routing",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
